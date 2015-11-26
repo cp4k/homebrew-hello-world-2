8312430c1767cb9e3dbf9c703dabeb79185db5d6
Carter Sande <carter.sande@duodecima.technology>
2015-11-26 14:30:18 -0800

    Actually install Easygui instead of just unpacking it

--- hello-world-2.rb
+++ hello-world-2.rb
@@ -18,6 +18,11 @@ class HelloWorld2 < Formula
   
   def install
     resource("easygui").stage { system "python", *Language::Python.setup_install_args(libexec/"vendor") }
+    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
+    system "python", *Language::Python.setup_install_args(libexec)
+
+    bin.install Dir[libexec/"bin/*"]
+    bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
   end
   
   def caveats
