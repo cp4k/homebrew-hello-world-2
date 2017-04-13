94cd6aabba4aa3ec5660acccc79e6cbd85209679
Carter Sande <carter.sande@duodecima.technology>
2017-04-12 19:17:38 -0700

    Update formula to reflect qt name change

--- hello-world-2.rb
+++ hello-world-2.rb
@@ -1,15 +1,15 @@
 class HelloWorld2 < Formula
   desc "All the software you need to use Hello World!"
   homepage "http://helloworldbookblog.com"
-  url "https://github.com/cp4k/homebrew-hello-world-2.git", :using => :git, :revision => "1a0df8c7af730a2169069127a21b4795e9f89410"
+  url "https://github.com/cp4k/homebrew-hello-world-2.git", :using => :git, :revision => "5bd55d5bf1464b91ef6095885f9df978e0ba26c7"
   version "source"
-  revision 1
+  revision 2
   
   depends_on "python"
   depends_on "sdl_image"
   depends_on "cp4k/hello-world-2/sdl_mixer"
   depends_on "cp4k/hello-world-2/pygame"
-  depends_on "cartr/qt4/pyqt"
+  depends_on "cartr/qt4/pyqt@4"
   
   def install
     bin.install "finish-hello-world-2-installation.py"
