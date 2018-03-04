97d64b8b659ac9dd9850fea7142b2bb259b7efc9
Carter Sande <carter.sande@duodecima.technology>
2018-03-04 09:54:39 +0000

    Update installer Git hash/revision number

--- hello-world-2.rb
+++ hello-world-2.rb
@@ -1,9 +1,9 @@
 class HelloWorld2 < Formula
   desc "All the software you need to use Hello World!"
   homepage "http://helloworldbookblog.com"
-  url "https://github.com/cp4k/homebrew-hello-world-2.git", :using => :git, :revision => "3994ee69e6447ebfb8204d47018b310cc95b43a0"
+  url "https://github.com/cp4k/homebrew-hello-world-2.git", :using => :git, :revision => "5bd00ef661045a97c26f51c42d75a7b56418d2f7"
   version "source"
-  revision 5
+  revision 6
   
   depends_on "python@2"
   depends_on "cartr/qt4/pyqt@4"
