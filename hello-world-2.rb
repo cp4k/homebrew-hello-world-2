6533729caedd42b18af4b2d3438358932e3de41d
Carter Sande <carter.sande@duodecima.technology>
2018-03-04 09:03:20 +0000

    Update formula to reflect Python name shuffling

--- hello-world-2.rb
+++ hello-world-2.rb
@@ -1,11 +1,11 @@
 class HelloWorld2 < Formula
   desc "All the software you need to use Hello World!"
   homepage "http://helloworldbookblog.com"
-  url "https://github.com/cp4k/homebrew-hello-world-2.git", :using => :git, :revision => "308c311d700674ee93b19e713862d1224d186f4d"
+  url "https://github.com/cp4k/homebrew-hello-world-2.git", :using => :git, :revision => "3994ee69e6447ebfb8204d47018b310cc95b43a0"
   version "source"
-  revision 4
+  revision 5
   
-  depends_on "python"
+  depends_on "python@2"
   depends_on "cartr/qt4/pyqt@4"
   
   def install
